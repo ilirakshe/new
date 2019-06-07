73bb2e996443ccbeade898ff4feaee1775471e83
nexx <nexx83@gmail.com>
2019-06-07 15:12:48 +0300

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://nxx:ASDF2012gh@books-c9xaq.mongodb.net/test?retryWrites=true&w=majorityCopy",
+  googleClientID:
+    "421560241556-3n8hlppi6k40fh3ji6qr8dcd3enu1is1.apps.googleusercontent.com",
+  googleClientSecret: "Pp4jCtkhCDKyCUdnlcNaR8uw",
+};
---
+++ config/passport.js
@@ -0,0 +1,20 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("./keys");
+
+module.exports = function(passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "http://localhost:5000/auth/google/callback",
+        proxy: true,
+      },
+      (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken);
+        console.log(profile);
+      }
+    )
+  );
+};
